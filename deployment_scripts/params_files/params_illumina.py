93182849dd11319cf9a4202cda155913ac1de726
SantosJGND <dourado.jns@gmail.com>
2022-08-02 13:12:38 +0100

    add fast diamond to class parameters in illumina for test

--- deployment_scripts/params_files/params_illumina.py
+++ deployment_scripts/params_files/params_illumina.py
@@ -242,6 +242,7 @@ ARGS_CLASS = {
     },
     "diamond": {
         "DIAMOND_ARGS": [
+            "--top 5 -e 0.01 --id 40 --query-cover 40 --fast",
             "--top 3 -e 0.01 --id 50 --query-cover 50 --sensitive",
             "--top 3 -e 0.01 --id 70 --query-cover 60 --ultra-sensitive",
         ],
