c75fdafc72a04799b7f55993c91e1d795458ae1f
SantosJGND <dourado.jns@gmail.com>
2022-09-23 16:21:06 +0100

    incluide upper case phage in exclude descriptions

--- deployment_scripts/metagen_view/pathogen_detection/constants_settings.py
+++ deployment_scripts/metagen_view/pathogen_detection/constants_settings.py
@@ -13,7 +13,7 @@ class ConstantsSettings:
 
     project_directory = "/mnt/sdc/TELEVIR/projects/"
     static_directory = STATICFILES_DIRS[0]
-    exclude_descriptions = ["phage"]
+    exclude_descriptions = ["phage", "Phage"]
 
 
 class TestConstants:
