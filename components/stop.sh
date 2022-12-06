71e3219320eb15d1a00ec54d2620b2a1b17dd07e
SantosJGND <dourado.jns@gmail.com>
2022-12-06 12:47:07 +0000

    clean root directory.

--- components/stop.sh
+++
@@ -1,2 +0,0 @@
-echo "Stopping insaflu-server, postgres_backup and postgres containers ..."
-docker stop insaflu-server postgres_backup postgres
--- components/up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-# image name
-export IMAGE=insaflu-server
-
-docker-compose up ${IMAGE}
-
