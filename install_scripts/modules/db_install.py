665072f3eb1d1c0f8ed1cf9023c80abcc3cb71f9
SantosJGND <dourado.jns@gmail.com>
2023-02-04 10:41:15 +0000

    refseq_prot acc2taxid taxid to int

--- install_scripts/modules/db_install.py
+++ install_scripts/modules/db_install.py
@@ -689,7 +689,11 @@ class setup_dl:
         tax2description = pd.read_csv(f"{self.metadir}/taxid2desc.tsv", sep="\t")
 
         df = df.merge(tax2description, on="description", how="left")
+        print(df.head())
+        df = df[["prot_acc", "taxid"]]
+        df = df.dropna()
         df.taxid = df.taxid.astype(int)
+
         df.to_csv(outfile, sep="\t", index=False)
 
         self.meta["refseq_prot"] = outfile
