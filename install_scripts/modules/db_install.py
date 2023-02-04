e7da5eb9a95adeb4a5a84edd1eded5ab12a681f7
SantosJGND <dourado.jns@gmail.com>
2023-02-04 10:35:11 +0000

    include refseq_prot

--- install_scripts/modules/db_install.py
+++ install_scripts/modules/db_install.py
@@ -656,6 +656,7 @@ class setup_dl:
         """
 
         self.prot2taxid_rescue()
+        self.parse_refseq_prot()
 
     def parse_refseq_prot(self):
 
@@ -684,7 +685,7 @@ class setup_dl:
                     description = retrieve_within_square_brackets(line)
                     lines.append([acc, description])
 
-        df = pd.DataFrame(lines, columns=["acc", "description"])
+        df = pd.DataFrame(lines, columns=["prot_acc", "description"])
         tax2description = pd.read_csv(f"{self.metadir}/taxid2desc.tsv", sep="\t")
 
         df = df.merge(tax2description, on="description", how="left")
