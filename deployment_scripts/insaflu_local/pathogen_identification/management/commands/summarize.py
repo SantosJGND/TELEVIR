f7db430fff2c1e28ee9b658951eaa3f24e63fd94
SantosJGND <dourado.jns@gmail.com>
2023-02-18 18:35:06 +0000

    add raw reference extract to table in summarize

--- deployment_scripts/insaflu_local/pathogen_identification/management/commands/summarize.py
+++ deployment_scripts/insaflu_local/pathogen_identification/management/commands/summarize.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from pathogen_identification.models import (FinalReport, ParameterSet,
                                             PIProject_Sample, Projects,
-                                            RunMain)
+                                            RawReference, RunMain)
 from pathogen_identification.utilities.benchmark_graph_utils import \
     pipeline_tree
 from pathogen_identification.utilities.utilities_pipeline import (
@@ -64,16 +64,11 @@ def collect_parameters_project(project: Projects):
 
         for run in mainruns:
             ps = run.parameter_set
-            print(ps.leaf.pk)
             software_tree = ps.leaf.software_tree
             pipe_tree = all_pipe_trees_dict[software_tree.pk]
             all_paths = all_project_paths_dict[software_tree.pk]
 
-            print("node_leaf: ", pipe_tree.leaves_from_node(ps.leaf.index))
             leaf = pipe_tree.leaves_from_node(ps.leaf.index)[0]
-
-            print(all_paths.keys())
-            print(ps.leaf.index)
             params = all_paths.get(leaf, None)
 
             if params is None:
@@ -141,6 +136,69 @@ def projects_reports_summary(project_query):
     return all_reports
 
 
+def project_references(project_query):
+    all_params = []
+
+    for project in project_query:
+        project_params = collect_references_project(project)
+        if len(project_params):
+            all_params.append(project_params)
+
+    if len(all_params):
+        all_params = pd.concat(all_params)
+    return all_params
+
+
+def collect_references_project(project: Projects):
+    utils = Utils_Manager()
+    technology = project.technology
+    user = project.owner
+
+    samples = PIProject_Sample.objects.filter(project=project)
+    project_params = []
+
+    for sample in samples:
+        parameterset = ParameterSet.objects.filter(
+            project=project, sample=sample, status=ParameterSet.STATUS_FINISHED)
+        mainruns = RunMain.objects.filter(parameter_set__in=parameterset)
+
+        # get unique software trees:
+        software_trees = set([ps.leaf.software_tree for ps in parameterset])
+        all_project_paths_dict = {}
+        all_pipe_trees_dict = {}
+        for software_tree in software_trees:
+            pipe_tree = utils.parameter_util.software_tree_to_pipeline_tree(
+                software_tree=software_tree)
+            all_paths = pipe_tree.get_all_graph_paths()
+            all_project_paths_dict[software_tree.pk] = all_paths
+            all_pipe_trees_dict[software_tree.pk] = pipe_tree
+
+        for run in mainruns:
+
+            reference_mapped = RawReference.objects.filter(run=run)
+            reference_mapped = pd.DataFrame(reference_mapped.values())
+
+            ps = run.parameter_set
+
+            if reference_mapped is None:
+                print("no references found for run: ", run.pk)
+
+            reference_mapped["run"] = run.name
+            reference_mapped["run_id"] = run.pk
+            reference_mapped["sample"] = ps.sample.name
+            reference_mapped["sample_id"] = ps.sample.pk
+            reference_mapped["sample_name"] = ps.sample.name
+            reference_mapped["project"] = project.name
+            reference_mapped["project_id"] = project.pk
+
+            project_params.append(reference_mapped)
+
+    if len(project_params):
+        project_params = pd.concat(project_params)
+
+    return project_params
+
+
 class Command(BaseCommand):
     help = "deploy run"
 
@@ -197,45 +255,10 @@ class Command(BaseCommand):
                 name=options.get("project"), owner=user
             )
 
-        samples = PIProject_Sample.objects.filter(project__in=projects_query)
-        mainruns = RunMain.objects.filter(sample__in=samples)
-
-        reports = FinalReport.objects.filter(run__in=mainruns)
-
-        ####
-        projects = pd.DataFrame(projects_query.values()).rename(
-            {"name": "project_name"}, axis=1
-        )
-        samples = pd.DataFrame(samples.values()).rename(
-            {"name": "sample_name"}, axis=1)
-        mainruns = pd.DataFrame(mainruns.values()).rename(
-            {"name": "run_name"}, axis=1)
-        # rundetails= pd.DataFrame(rundetails.values())
-        reports = pd.DataFrame(reports.values()).rename(
-            {"id": "report_id"}, axis=1)
-
-        ####
-        reports_df = (
-            pd.merge(mainruns, reports, left_on="id", right_on="run_id")
-            .drop("id", axis=1)
-            .drop("sample_id_y", axis=1)
-            .rename({"sample_id_x": "sample_id"}, axis=1)
-        )
-
-        reports_df = (
-            pd.merge(samples, reports_df, left_on="id", right_on="sample_id")
-            .drop("id", axis=1)
-            .drop("project_id_y", axis=1)
-            .rename({"project_id_x": "project_id"}, axis=1)
-        )
-        reports_df = pd.merge(
-            projects, reports_df, left_on="id", right_on="project_id"
-        ).drop("id", axis=1)
-
-        ####
         # local_tree= self.generate_hardcoded_pipeline_tree(technology=technology)
         all_parameters = projects_params_summary(projects_query)
         reports_df = projects_reports_summary(projects_query)
+        references_df = project_references(projects_query)
 
         ####
         reports_df.to_csv("all_reports.tsv", index=False, sep="\t")
@@ -243,3 +266,5 @@ class Command(BaseCommand):
         ####
         all_parameters.to_csv("all_parameters.tsv", index=False, sep="\t")
         #
+
+        references_df.to_csv("all_references.tsv", index=False, sep="\t")
