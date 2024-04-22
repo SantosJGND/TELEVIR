38049812bf786534efac11a363e34d16128a5f62
SantosJGND <dourado.jns@gmail.com>
2024-04-22 14:34:38 +0100

    remove deploy in main

--- main.py
+++ main.py
@@ -200,27 +200,6 @@ def main():
     ############
     ############
 
-    if args.deploy:
-        from deployment_scripts.main_deployment_setup import main_deploy_prep
-
-        # begin by installing televir deployment requirements
-        metagen_prep.setup_deploy()
-
-        deploy_prep = main_deploy_prep(pdir=CWD + "/deployment_scripts/")
-        deploy_prep.object_input(
-            DEPLOYMENT_DIR,
-            ENVDIR,
-            metagen_prep.wdir.dbdir,
-            metagen_prep.wdir.seqdir,
-            metagen_prep.wdir.metadir,
-            SOURCE_DIR,
-            tech=TECH,
-            docker_home=HOME,
-        )
-
-        # deploy_prep.dir_prep()
-        deploy_prep.export(DATABASE)
-
 
 if __name__ == "__main__":
     import logging
