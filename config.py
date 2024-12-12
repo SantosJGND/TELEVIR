f922d8e949d3a247766e21fb7e25cfa3e999f7bd
SantosJGND <dourado.jns@gmail.com>
2024-12-12 16:50:58 +0000

    taxdump removed erronwously

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
-
+##
 # replace the variables bellow with paths in your system. Beware of permissions.
 
 SOURCE = (
     # path to miniconda3 executable.
     "/opt/conda/etc/profile.d/conda.sh"
 )
+# /home/bioinf/Desktop/METAGEN/depo
 # path to the directory where the databases will be stored.
 HOME = "/televir/mngs_benchmark/"
 # path to the directory where the environments will be stored.
 ENVDIR = "/televir/mngs_benchmark/mngs_environments/"
-# Updatae
+# technology used to generate the database and deployment files. options: "illumina" or "nanopore".
+# path to the taxdump.tar.gz file.
+TAXDUMP = "/opt/taxdump.tar.gz"
 UPDATE = False
